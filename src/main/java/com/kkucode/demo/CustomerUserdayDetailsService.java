b69ff8c2d88cd597fccd026251deaa21911d5db2
Dudumanga <sr_chaitawat@kkumail.com>
2021-10-26 10:24:07 -0400

    sr in

--- src/main/java/com/kkucode/demo/CustomerUserdayDetailsService.java
+++ src/main/java/com/kkucode/demo/CustomerUserdayDetailsService.java
@@ -1,5 +1,5 @@
 package com.kkucode.demo;
-
+//sr
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
